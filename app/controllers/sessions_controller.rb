d261acbbad0274865eef45c6d883b9d9aa969a8a
Rob <github email address>
2018-01-03 11:45:34 -0500

    omniauth working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,58 @@
 class SessionsController < ApplicationController
 
-  def new
-    
+  def new  
   end
 
   def create
-    #HTTP is a stateless protocol
-    ##Every request is independent
-    user = User.find_by(:email => params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-
+    
+    if auth_hash = request.env["omniauth.auth"]
+      # raise "Exiting User Loggin In Via Github".inspect
+      user = User.find_or_create_by_omniauth(auth_hash)
+      session[:user_id] = user.id 
       redirect_to root_path
     else
-      render 'sessions/new'
+      #normal login
+      user = User.find_by(:email => params[:email])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id 
+        redirect_to root_path
+      else 
+        render 'sessions/new'
+      end 
     end
   end
+  # BEFORE REFACTORING, lots used in the find_or_create_by_omniauth method in User model
+  # def create
+  #   #HTTP is a stateless protocol
+  #   ##Every request is independent
+  #   if auth_hash =  request.env["omniauth.auth"] #they logged in via omniauth
+  #     #persons 100% trusted email coming form github
+  #     oauth_email = request.env["omniauth.auth"]["info"]["email"] 
+  #     #I've seen this user beore, log them in because I know there is no way they spoofed their email
+  #     if user = User.find_by(:email => oauth_email)
+  #       # raise "Existing User Loggin In Via Github".inspect
+  #       session[:user_id] = user.id 
+  #     else
+  #       # raise "New User Loggin In Via Github".inspect
+  #       #I know who the person is but this is the first time on my app
+  #       user = User.new(:email => oauth_email, :password => SecureRandom.hex) #I could also store the provider and their UserID on that provider
+  #       if user.save
+  #         session[:user_id] = user.id 
+  #       else
+  #         raise user.errors.full_messages.inspect
+  #       end
+  #     end
+  #   else  #normal login
+  #     user = User.find_by(:email => params[:email])
+  #     if user && user.authenticate(params[:password])
+  #       session[:user_id] = user.id
+
+  #       redirect_to root_path
+  #     else
+  #       render 'sessions/new'
+  #     end
+  #   end
+  # end
 
   def destroy
     reset_session
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get 'items/create'
+  
+  get '/login' => "sessions#new"
+  post '/sessions' => "sessions#create"
+  get '/logout' => "sessions#destroy"
+  get '/auth/:provider/callback' => 'sessions#create'
+
+  resources :users, only: [:new, :create]
 
   resources :lists do
     resources :items
   end
 
-  resources :users, only: [:new, :create]
-
-  get '/login' => "sessions#new"
-  post '/sessions' => "sessions#create"
-  get '/logout' => "sessions#destroy"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
